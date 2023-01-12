4f95769c2706100336d9efbd46cb53d515602bab
filipe dias <filipediasit@gmail.com>
2023-01-12 21:15:16 +0000

    the login part is done and will use the switch method instead of the if , else if statment , but since I'm doing one by one right now its simpler for me to not be confused

--- src/App.java
+++ src/App.java
@@ -8,17 +8,39 @@ public class App {
     }
 
     private void init() throws FileNotFoundException {
-
+      Scanner scanner = new Scanner(System.in);
+      boolean logged = false;
+      while(true) {
         System.out.println("Welcome to the mega GradeManager 3000!\n" + "To use this manager you need to login:");
         System.out.println("Username:");
-        Scanner scanner = new Scanner(System.in);
         final var username = scanner.nextLine();
         System.out.println("Password:");
         final var password = scanner.nextLine();
         Professor.authenticate(username, password);
-
-        //avaliar o resultado de Professor.authenticate e agir de acordo
-        //continuação do programa
-
-    }
+          if (Professor.authenticate(username, password)) {
+              logged = true;
+              System.out.println("Login Successful!");
+              break;
+          } else {
+              System.out.println("Invalid Login!");
+          }
+      }
+    System.out.println(logged ? "What do you need to do?" : "");
+    System.out.println("1. Add a new Class");
+    System.out.println("2. Manage a class");
+    System.out.println("3. Exit");
+      final var getUserResponse = scanner.nextLine();
+      if (getUserResponse.equals("1")) {
+          System.out.println("Adding a new Class");x\
+          System.out.println("Class name:");
+          String UserClass = scanner.nextLine();
+          System.out.println("Description:");
+          String UserClassDescription = scanner.nextLine();
+          System.out.println("You ve created a new class called " + "\""
+                             + UserClass + " " + UserClassDescription + "\"");
+      }
+      else if(getUserResponse.equals("2")) {
+          System.out.println("What class do you want to manage?");
+      }
+  }
 }
