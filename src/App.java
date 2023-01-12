66ac27dbe93de41e220112df865c131da939a0ab
filipe dias <filipediasit@gmail.com>
2023-01-12 22:30:08 +0000

    fixed the loop login and changed the loop in the first menu.

--- src/App.java
+++ src/App.java
@@ -10,6 +10,7 @@ public class App {
     private void init() throws FileNotFoundException {
       Scanner scanner = new Scanner(System.in);
       boolean logged = false;
+      outerloop:
       while(true) {
         System.out.println("Welcome to the mega GradeManager 3000!\n" + "To use this manager you need to login:");
         System.out.println("Username:");
@@ -20,27 +21,32 @@ public class App {
           if (Professor.authenticate(username, password)) {
               logged = true;
               System.out.println("Login Successful!");
-              break;
           } else {
               System.out.println("Invalid Login!");
           }
-      }
-    System.out.println(logged ? "What do you need to do?" : "");
-    System.out.println("1. Add a new Class");
-    System.out.println("2. Manage a class");
-    System.out.println("3. Exit");
-      final var getUserResponse = scanner.nextLine();
-      if (getUserResponse.equals("1")) {
-          System.out.println("Adding a new Class");x\
-          System.out.println("Class name:");
-          String UserClass = scanner.nextLine();
-          System.out.println("Description:");
-          String UserClassDescription = scanner.nextLine();
-          System.out.println("You ve created a new class called " + "\""
-                             + UserClass + " " + UserClassDescription + "\"");
-      }
-      else if(getUserResponse.equals("2")) {
-          System.out.println("What class do you want to manage?");
+          while (true) {
+              System.out.println(logged ? "What do you need to do?" : "");
+              System.out.println("1. Add a new Class");
+              System.out.println("2. Manage a class");
+              System.out.println("3. Exit");
+              final var getUserResponse = scanner.nextLine();
+              if (getUserResponse.equals("1")) {
+                  System.out.println("Adding a new Class");
+                  System.out.println("Class name:");
+                  String UserClass = scanner.nextLine();
+                  System.out.println("Description:");
+                  String UserClassDescription = scanner.nextLine();
+                  System.out.println("You've created a new class called " + "\""
+                          + UserClass + " " + UserClassDescription + "\"");
+              } else if (getUserResponse.equals("2")) {
+                  System.out.println("What class do you want to manage?");
+              } else if (getUserResponse.equals("3")) {
+                  System.out.println("Quiting");
+                  break outerloop;
+              } else {
+                  System.out.println("Invalid command, try again!");
+              }
+          }
       }
   }
 }
