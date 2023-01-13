4b27c6af09e386c2e92505c7c59fe3493f7f5b56
filipe dias <filipediasit@gmail.com>
2023-01-13 23:08:21 +0000

    organized and fixed the login

--- src/App.java
+++ src/App.java
@@ -9,43 +9,41 @@ public class App {
 
     private void init() throws FileNotFoundException {
       Scanner scanner = new Scanner(System.in);
-      boolean logged = false;
-      outerloop:
-      while(true) {
-        System.out.println("Welcome to the mega GradeManager 3000!\n" + "To use this manager you need to login:");
-        System.out.println("Username:");
-        final var username = scanner.nextLine();
-        System.out.println("Password:");
-        final var password = scanner.nextLine();
-        Professor.authenticate(username, password);
-          if (Professor.authenticate(username, password)) {
-              logged = true;
-              System.out.println("Login Successful!");
-          } else {
-              System.out.println("Invalid Login!");
-          }
-          while (true) {
-              System.out.println(logged ? "What do you need to do?" : "");
-              System.out.println("1. Add a new Class");
-              System.out.println("2. Manage a class");
-              System.out.println("3. Exit");
-              final var getUserResponse = scanner.nextLine();
-              if (getUserResponse.equals("1")) {
+      System.out.println("Welcome to the mega GradeManager 3000!\n" + "To use this manager you need to login:");
+      String username;
+      String password;
+      boolean userCredentials = false;
+      while(!userCredentials) {
+          System.out.println("Username:");
+          username = scanner.nextLine();
+          System.out.println("Password:");
+          password = scanner.nextLine();
+          userCredentials = Professor.authenticate(username, password);
+      }
+      while (true) {
+          System.out.println("What do you need to do?");
+          System.out.println("1. Add a new Class");
+          System.out.println("2. Manage a class");
+          System.out.println("3. Exit");
+          final var getUserResponse = scanner.nextLine();
+          switch (getUserResponse) {
+              case "1":
                   System.out.println("Adding a new Class");
                   System.out.println("Class name:");
                   String UserClass = scanner.nextLine();
                   System.out.println("Description:");
                   String UserClassDescription = scanner.nextLine();
                   System.out.println("You've created a new class called " + "\""
-                          + UserClass + " " + UserClassDescription + "\"");
-              } else if (getUserResponse.equals("2")) {
+                          + UserClass + " " + UserClassDescription + "\" \n");
+                  break;
+              case "2":
                   System.out.println("What class do you want to manage?");
-              } else if (getUserResponse.equals("3")) {
-                  System.out.println("Quiting");
-                  break outerloop;
-              } else {
+                  break;
+              case "3":
+                  System.out.println("Quiting \n");
+                  System.exit(0);
+              default:
                   System.out.println("Invalid command, try again!");
-              }
           }
       }
   }
